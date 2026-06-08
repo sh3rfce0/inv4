a8357357002dba5d6a1ecb3abfcc58171ff32147
root <root@lasvegas-nv-datacenter.serverpoint.com>
2026-06-09 03:52:31 +0800

    Add all script assets to repo

--- new-izin-api/scripts/install.sh
+++ new-izin-api/scripts/install.sh
@@ -531,7 +531,7 @@ apt install rclone -y
 printf "q\n" | rclone config
 wget -O /root/.config/rclone/rclone.conf "${REPO}Cfg/rclone.conf"
 cd /bin
-git clone https://github.com/sh3rfce0/wondershaper.git
+git clone https://github.com/sheriflks/wondershaper.git
 cd wondershaper
 sudo make install
 cd
